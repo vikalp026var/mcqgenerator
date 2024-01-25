20e12e94ba0557c38eaafc0d4de245383ae06b32
VIKALP <113850666+vikalp026var@users.noreply.github.com>
2024-01-25 20:17:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
-from setuptools import setup
+from setuptools import find_packages, setup
+
+setup(
+     name="MCQ Generator",
+     version="0.0.1",
+     author="Vikalp",
+     author_email="vikalp026varshney@gmail.com",
+     packages=find_packages(),
+     include_dirs=[]
+)
